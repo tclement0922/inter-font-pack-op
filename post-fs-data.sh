735f0109701d1808c6962e2422dead03c45474c6
T. Clement <50289872+tclement0922@users.noreply.github.com>
2021-09-24 19:48:18 +0200

    Fixed the rectangle on the lock screen !
    The fallback fonts were not applied to the fonts_base.xml file

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -4,3 +4,5 @@ MODPATH="/data/adb/modules/inter-font-op"
 
 sed 's/<\/familyset>//g' /system/etc/fonts.xml | cat - $MODPATH/fallback.xml > $MODPATH/system/etc/fonts.xml
 
+sed 's/<\/familyset>//g' /system/etc/fonts_base.xml | cat - $MODPATH/fallback.xml > $MODPATH/system/etc/fonts_base.xml
+
